7b31e65ebb2dd53ca47771160055c20201fbe413
WesongaJackson <hackingson2@gmail.com>
2024-01-20 12:41:14 +0300

    Initial commit

---
+++ Aws_S3_buckets/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for Aws_S3_buckets project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/5.0/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Aws_S3_buckets.settings')
+
+application = get_wsgi_application()
